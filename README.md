db0e27643621918159ee8d1d3acc08d6323c3a14
Votex <nikitossoso4ka@gmail.com>
2024-10-20 21:51:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flixel-screenshot-fnf
+ 
---
+++ src/screenshotplugin/ScreenShotPlugin.hx
@@ -0,0 +1,4 @@
+package screenshotplugin;
+
+@:deprecated("screenshotplugin.ScreenShotPlugin is deprecated, use flixel.addons.plugin.ScreenShotPlugin instead")
+typedef ScreenShotPlugin = flixel.addons.plugin.ScreenShotPlugin;
